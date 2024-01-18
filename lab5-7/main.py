ac2e0507bfa89da065ec1d940c6604da7c7a0372
StetcoClaudiu <44034559+StetcoClaudiu@users.noreply.github.com>
2024-01-19 01:42:38 +0200

    Update main.py

--- lab5-7/main.py
+++ lab5-7/main.py
@@ -1,43 +1,37 @@
 from Grammar import Grammar
 from Parser import Parser
-from ParserOutput import ParserOutput
-
+from ParserOutput import ParsingOutput
 
 def main():
     while True:
         print("Choose an option:")
         print("1. Run Parser 1")
         print("2. Run Parser 2")
-        print("3. Run Parser 3")
         print("0. Quit")
 
         choice = input("Enter your choice: ")
 
         if choice == '1':
-            sequence_file = "seq1.txt"
-            grammar_file = "g1.txt"
+            sequence_file = "sequence1.txt"
+            grammar_file = "grammar1.txt"
         elif choice == '2':
-            sequence_file = "seq2.txt"
-            grammar_file = "g2.txt"
-        elif choice == '3':
-            sequence_file = "seq3.txt"
-            grammar_file = "g3.txt"
+            sequence_file = "sequence2.txt"
+            grammar_file = "grammar2.txt"
         elif choice == '0':
             break
         else:
             print("Invalid choice. Please select a valid option.")
             continue
 
-        grammar = Grammar()
-        grammar.read_from_file(grammar_file)
+        cfg_grammar = Grammar()
+        cfg_grammar.read_from_file(grammar_file)
 
-        parser = Parser(grammar, sequence_file)
-        parser.run()
+        parser = Parser(cfg_grammar, sequence_file)
+        parser.execute()
 
-        parser_output = ParserOutput(grammar, sequence_file)
-        parser_output.create_parsing_tree(parser.working)
-        parser_output.write_parsing_tree(parser.state, parser.working, "tree.txt")
-
+        parsing_output = ParsingOutput(cfg_grammar, sequence_file)
+        parsing_output.build_tree(parser.working_stack)
+        parsing_output.write_tree(parser.state, parser.working_stack, "tree.txt")
 
 if __name__ == "__main__":
     main()
